33d3aa8f7b2e951c58e670b9b384d2b87bb10321
Akinwande Tomisin <tomjames156@gmail.com>
2023-07-24 14:58:45 +0100

    updated settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -190,4 +190,8 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS =  [
+    config('TRUSTED_ORIGIN'),
+    config('DEFAULT_ORIGIN'),
+    config('DEV_ORIGIN')
+]
